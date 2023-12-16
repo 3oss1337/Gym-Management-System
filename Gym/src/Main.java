855378ea8351128445c166376075a44b6287f2c4
AhmedAlaa248 <ahmed.elrweney@yahoo.com>
2023-12-17 00:55:24 +0200

    transfer some FNS

--- Gym/src/Main.java
+++ Gym/src/Main.java
@@ -34,17 +34,17 @@ public class Main {
 
         Customer c2 = new Customer("Ahmed", "Tallo", "123", "123", "123", "123", 12,s1);
 */
-        /*Coach coach = new Coach("salah","sss","M","address","01111111","adsa@gmail.com",30);
+/*Coach coach = new Coach("salah","sss","M","address","01111111","adsa@gmail.com",30);
         inbody.knowNeeded(c2);
         c2.addToInBodyHistory(inbody);
         coach.callInBodyHistory(c2);
 */
-        //m1.calculateTotalPrice(3, m1.calcMonthPrice(6));
-        //Gym.addSubscription(s1);
-        //c2.displayMemberShipDetails();
-        //System.out.println(inbody.canPerformInBody(c2));
-        //c2.performInBody(inbody);
-        //System.out.println(inbody.canPerformInBody(c2));
+/*        m1.calculateTotalPrice(3, m1.calcMonthPrice(6));
+        Gym.addSubscription(s1);
+        c2.displayMemberShipDetails();
+        System.out.println(inbody.canPerformInBody(c2));
+        c2.performInBody(inbody);
+        System.out.println(inbody.canPerformInBody(c2));*/
 
 
     }
